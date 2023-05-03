24b070e3f7819e4da482de2ceb98e0e69edb112d
Will Lee <will.maps@gmail.com>
2023-05-03 12:49:16 -0400

    Fix up clan war versus view

--- force-app/main/default/lwc/clanWarVersusView/clanWarVersusView.js
+++ force-app/main/default/lwc/clanWarVersusView/clanWarVersusView.js
@@ -1,13 +1,17 @@
 import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
 import { LightningElement, api, wire } from 'lwc';
-import CLAN_WAR_NAME_FIELD from '@salesforce/schema/ClanWar__c.Name';
 import getWarDetails from '@salesforce/apex/ClanWarVersusViewController.getWarDetails';
+import syncClanWarLeagueWar from '@salesforce/apex/ClanWarSyncer.syncClanWarLeagueWar';
+import CLAN_WAR_NAME_FIELD from '@salesforce/schema/ClanWar__c.Name';
+import WAR_TAG_FIELD from '@salesforce/schema/ClanWar__c.WarTag__c';
 
 export default class ClanWarVersusView extends LightningElement {
     @api recordId; // clan war id
-    clanWar;
 
-    @wire(getRecord, { recordId: '$recordId', fields: [CLAN_WAR_NAME_FIELD]})
+    clanWar;
+    warDetails;
+
+    @wire(getRecord, { recordId: '$recordId', fields: [CLAN_WAR_NAME_FIELD, WAR_TAG_FIELD]})
     processGetRecord({ data, error }) {
         if (error) {
             console.error(error);
@@ -30,6 +34,23 @@ export default class ClanWarVersusView extends LightningElement {
 
     get clanWarName() {
         return getFieldValue(this.clanWar, CLAN_WAR_NAME_FIELD);
+    }
+
+    async refreshWarDetails() {
+        const warTag = getFieldValue(this.clanWar, WAR_TAG_FIELD);
+        this.isProcessing = true;
+        if (warTag) {
+            try {
+                await syncClanWarLeagueWar({ warTag });
+            } catch (error) {
+                console.error(error);
+                this.isProcessing = false;
+                return;
+            }
+        } else {
+            console.log('implement reg clan war refresh');
+        }
+        this.isProcessing = false;
     }
 
 }
